2173823635c558b622f3540c915a772c0c6a704a
Kasper de Boer <kasper@kasper.dev>
2023-04-30 23:35:54 +0200

    Setup module model + controller

---
+++ keuzewijzer-hbo-deeltijd-ict-API/Controllers/ModuleController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using keuzewijzer_hbo_deeltijd_ict_API.Dal;
+using keuzewijzer_hbo_deeltijd_ict_API.Models;
+
+namespace keuzewijzer_hbo_deeltijd_ict_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModuleController : ControllerBase
+    {
+        private readonly KeuzewijzerContext _context;
+
+        public ModuleController(KeuzewijzerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Module
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Module>>> GetModules()
+        {
+          if (_context.Modules == null)
+          {
+              return NotFound();
+          }
+            return await _context.Modules.ToListAsync();
+        }
+
+        // GET: api/Module/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Module>> GetModule(int id)
+        {
+          if (_context.Modules == null)
+          {
+              return NotFound();
+          }
+            var @module = await _context.Modules.FindAsync(id);
+
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return @module;
+        }
+
+        // PUT: api/Module/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModule(int id, Module @module)
+        {
+            if (id != @module.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@module).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModuleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Module
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Module>> PostModule(Module @module)
+        {
+          if (_context.Modules == null)
+          {
+              return Problem("Entity set 'KeuzewijzerContext.Modules'  is null.");
+          }
+            _context.Modules.Add(@module);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetModule", new { id = @module.Id }, @module);
+        }
+
+        // DELETE: api/Module/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModule(int id)
+        {
+            if (_context.Modules == null)
+            {
+                return NotFound();
+            }
+            var @module = await _context.Modules.FindAsync(id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            _context.Modules.Remove(@module);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ModuleExists(int id)
+        {
+            return (_context.Modules?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
