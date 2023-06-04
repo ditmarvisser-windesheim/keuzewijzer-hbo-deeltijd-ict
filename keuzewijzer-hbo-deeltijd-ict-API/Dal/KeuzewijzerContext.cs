4bf2657f8d3eea773d21743a74b4b0843bd4d564
BoVerzaal <Bo.Verzaal@topicus.nl>
2023-06-04 16:27:52 +0200

    DatabaseUpdate

--- keuzewijzer-hbo-deeltijd-ict-API/Dal/KeuzewijzerContext.cs
+++ keuzewijzer-hbo-deeltijd-ict-API/Dal/KeuzewijzerContext.cs
@@ -661,10 +661,13 @@ namespace keuzewijzer_hbo_deeltijd_ict_API.Dal
 
 
             modelBuilder.Entity<StudyRouteItem>().HasData(
-                new StudyRouteItem { Id = 1, Year = 2023, Semester = 1, StudyRouteId = 1, SemesterItemId = 1 },
-                new StudyRouteItem { Id = 2, Year = 2023, Semester = 1, StudyRouteId = 1, SemesterItemId = 2 },
-                new StudyRouteItem { Id = 3, Year = 2023, Semester = 1, StudyRouteId = 1, SemesterItemId = 3 },
-                new StudyRouteItem { Id = 4, Year = 2023, Semester = 1, StudyRouteId = 1, SemesterItemId = 4 }
+                new StudyRouteItem { Id = 1, Year = 1, Semester = 1, StudyRouteId = 1, SemesterItemId = 1 },
+                new StudyRouteItem { Id = 2, Year = 1, Semester = 2, StudyRouteId = 1, SemesterItemId = 2 },
+                new StudyRouteItem { Id = 3, Year = 2, Semester = 1, StudyRouteId = 1, SemesterItemId = 3 },
+                new StudyRouteItem { Id = 4, Year = 2, Semester = 2, StudyRouteId = 1, SemesterItemId = 4 },
+                new StudyRouteItem { Id = 5, Year = 3, Semester = 1, StudyRouteId = 1, SemesterItemId = 5 },
+                new StudyRouteItem { Id = 6, Year = 3, Semester = 2, StudyRouteId = 1, SemesterItemId = 6 },
+                new StudyRouteItem { Id = 7, Year = 4, Semester = 1, StudyRouteId = 1, SemesterItemId = 7 }
                 );
 
         }
