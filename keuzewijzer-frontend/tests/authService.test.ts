c887451f9ddf63c07991f0e2163b7e9d9ca01d53
Kasper de Boer <kasper@kasper.dev>
2023-06-13 22:46:54 +0200

    Added Jest + authservice test

---
+++ keuzewijzer-frontend/tests/authService.test.ts
@@ -0,0 +1,28 @@
+import AuthService from '../src/services/AuthService';
+
+test('login should authenticate the user and set the access token and user data', async () => {
+  // Arrange
+  const authService = new AuthService();
+  const username = 'testUser';
+  const password = 'testPassword';
+
+  // Mock the login function
+  const mockLogin = jest.spyOn(authService, 'login').mockResolvedValue({
+    status: 200,
+    message: 'Login successful',
+  });
+
+  // Act
+  const loginResult = await authService.login(username, password);
+
+  // Assert
+  expect(mockLogin).toHaveBeenCalledWith(username, password);
+  // Add assertions for the expected behavior based on the login result
+  expect(loginResult).toEqual({
+    status: 200,
+    message: 'Login successful',
+  });
+
+  // Restore the mock
+  mockLogin.mockRestore();
+});
